9e2433feb3961959c0550c16708a2a37dd9cea5d
Vishal Chaudhari <122671523+vishal0714@users.noreply.github.com>
2025-09-03 20:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+ let myAge = document.querySelector('#age')
+    let myName = document.querySelector('#name')
+    let submit = document.querySelector('#btn')
+
+    submit.addEventListener('click', () => {
+        if (myAge.value.trim() === "" || myName.value.trim() === "") {
+            alert("Please enter valid details.")
+        }
+
+        myPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (myAge.value > 18) {
+                    resolve(("Welcome, . You can vote."))
+                } else {
+                    reject(("Oh sorry . You aren't old enough."))
+                }
+            }, 4000)
+
+        })
+
+        myPromise
+            .then(message => alert(message))
+            .catch(message => alert(message))
+    })
